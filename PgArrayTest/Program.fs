cee57d8213c4846ca725d774bd799b2d508b90b7
Felix <i@lcz.link>
2022-03-14 17:33:14 +0800

    remove tryGet

--- PgArrayTest/Program.fs
+++ PgArrayTest/Program.fs
@@ -32,13 +32,10 @@ let main args =
 
     db.Database.EnsureCreated()
 
-    let tryGet (guids: Guid[]) =
-        db.Events.Where(fun x -> guids.Contains(x.Id)).ToList()
+    let guids = [| Guid("ab02c141-7706-4570-8893-ae60fa741f81"); Guid("0e89bc4e-63dd-4cb4-be17-7108d3e7a90b") |]
 
-    let guids =  [| Guid("ab02c141-7706-4570-8893-ae60fa741f81"); Guid("0e89bc4e-63dd-4cb4-be17-7108d3e7a90b") |]
+    db.Events.Where(fun x -> guids.Contains(x.Id)).ToList()
 
-    do tryGet guids
-    
     0
 
 
